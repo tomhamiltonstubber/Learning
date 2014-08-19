ce0759148563ecea3d88790808e49c3ea8a13c88
tomhamiltonstubber <tomhamiltonstubber@gmail.com>
2014-08-19 17:37:47 +0100

    small changes to user.py

--- learning/TicTacToe/user.py
+++ learning/TicTacToe/user.py
@@ -44,33 +44,3 @@ class Players:
                 else:
                     player = "p1"
         return True
-        #             #User
-        # if player == "p1":
-        #     sym = "X"
-        #     self.myboard.print_board()
-        #     pos = raw_input("Where would you like to go?")
-        #     if pos in ["1","2","3","4","5","6","7","8","9"]:
-        #         if self.myboard.check_for_space(pos,sym) == False:
-        #             print("That space is taken!")
-        #             print("Please choose another:")
-        #             self.player_turn("p1")
-        #     else:
-        #         print("Please enter a number between 1 and 9")
-        #         self.player_turn("p1")
-        #     if self.myboard.check_board(sym) == True:
-        #         print("before")
-        #         return True
-        #     self.player_turn("p2")
-
-        # #AI    
-        # elif player == "p2":
-        #     pos = randrange(1,10)
-        #     sym = "O"
-        #     if self.myboard.check_for_space(pos,sym) == False:
-        #         self.player_turn("p2")
-        #     self.myboard.print_board()
-        #     print("The AI went in space " + str(pos))
-        #     if self.myboard.check_board(sym) == True:
-        #         print("before")
-        #         return True
-        #     self.player_turn("p1")

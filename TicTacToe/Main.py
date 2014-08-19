93e53028aa9320b361dace664ffe6e06ae0eab4e
tomhamiltonstubber <tomhamiltonstubber@gmail.com>
2014-08-19 13:52:11 +0100

    Worked on windows file

--- TicTacToe/Main.py
+++ TicTacToe/Main.py
@@ -1,7 +1,7 @@
 import sys
 import random
 
-    #CLASS FOR THE BOARD
+    #Class for the board
 class gameboard(object):
     board = ["1","2","3","4","5","6","7","8","9"]
     v = "|"
@@ -10,7 +10,7 @@ class gameboard(object):
     def __init__(self,name):
         self.name = name
         
-        #CHECK IF THE SPACE IS FREE OTHERWISE RETURN TO PLAYER
+        #Check to see if the space is free
     def check_for_space(self,position,symbol):
         if self.board[int(position)-1] == "X" or self.board[int(position)-1] == "O":
             if symbol == "O":
@@ -22,7 +22,7 @@ class gameboard(object):
         else:
             self.add_turn(position,symbol)
             
-        #ADD THE TURN TO THE BOARD LIST, THEN GO TO NEXT PLAYER
+        #Change the board space to the appropriate symbol
     def add_turn(self, position, symbol):
         self.board[int(position)-1] = symbol
         self.print_board()
@@ -33,7 +33,7 @@ class gameboard(object):
             print("The AI went in space " + str(position))
             playersturn.player_1_turn()
         
-        #PRINT THE BOARD
+        #Print the board
     def print_board(self):
         print(self.board[0] + self.v + self.board[1] + self.v + self.board[2])
         print(self.h + self.h + self.h + self.h + self.h)
@@ -42,7 +42,7 @@ class gameboard(object):
         print(self.board[6] + self.v + self.board[7] + self.v + self.board[8])
         print("")
     
-        #CHECK TO SEE IF THE BOARD IS FULL OR 3-IN-A-ROW
+        #Check to see if game is over
     def check_board(self, symbol):
         if (self.board[0] == self.board[1] == self.board[2]) or (self.board[3] == self.board[4] == self.board[5]) or (self.board[6] == self.board[7] == self.board[8]) or (self.board[0] == self.board[3] == self.board[6]) or (self.board[1] == self.board[4] == self.board[7]) or (self.board[2] == self.board[5] == self.board[8]) or (self.board[0] == self.board[4] == self.board[8]) or (self.board[2] == self.board[4] == self.board[6]):
             if symbol == "X":
@@ -56,12 +56,12 @@ class gameboard(object):
             end_game()
 
     
-    #CLASS FOR THE PLAYER'S            
+    #Class for players         
 class players(object):
     def __init__(self,name):
         self.name = name
     
-        #DECIDE WHO GOES FIRST
+        #Decide who goes first
     def decide_first_go(self):
         if random.randrange(1,3) == 1:
             print("The AI will go first")
@@ -71,7 +71,7 @@ class players(object):
             addtoboard.print_board()
             self.player_1_turn()
             
-        #PLAYER 1'S GO
+        #Player 1'S go
     def player_1_turn(self):
         pos = raw_input("Where would you like to go?")
         if pos in ["1","2","3","4","5","6","7","8","9"]:
@@ -81,13 +81,13 @@ class players(object):
             print("Please enter a number between 1 and 9")
             self.player_1_turn()
     
-        #AI's GO
+        #AI's go
     def player_2_turn(self):
         pos = random.randrange(1,10)
         sym = "O"
         addtoboard.check_for_space(pos,sym)
         
-        #DECIDE WHETHER TO PLAY AGAIN, ELSE END
+        #Player decide whether or not to play again
 def end_game():
     goagain = raw_input("Would you like to try again? Y/N?")
     if (goagain == 'y') or (goagain == 'Y'):
